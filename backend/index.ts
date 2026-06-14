04a9688ea42639ef0f742c54dbf8eb66868c5db7
Vikaskanukuntla <vikaskanukuntla87@gmail.com>
2026-06-14 20:49:42 +0530

    updated

--- backend/index.ts
+++ backend/index.ts
@@ -4,10 +4,12 @@ import axios from "axios";
 import cors from "cors"
 import { scrapeGithub } from "./scrapers/github";
 import { prisma } from "./db";
+import { initSideBand } from "./sideband";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.text({ type: ["application/sdp", "text/plain"] }));
 
 
 app.post("/api/v1/pre-interview" , async (req,res) => {
@@ -39,11 +41,46 @@ app.post("/api/v1/pre-interview" , async (req,res) => {
         res.json({
             id : interview.id
         })
-        
-
-
-
 })
+
+app.post("/api/v1/session/:interviewId" , async (req , res) => {
+
+    const sessionConfig = JSON.stringify({
+        type: "realtime",
+        model: "gpt-realtime-2",
+        audio: { output: { voice: "marin" } },
+      });
+
+
+      const fd = new FormData();
+      fd.set("sdp", req.body);
+      fd.set("session", sessionConfig);
+    
+      try {
+        const sdpResponse = await fetch("https://api.openai.com/v1/realtime/calls", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
+            "OpenAI-Safety-Identifier": "hashed-user-id",
+          },
+          body: fd,
+        });
+        // Location: /v1/realtime/calls/rtc_123456
+        const location = sdpResponse.headers.get("Location");
+        const callId = location?.split("/").pop()!;
+        console.log(callId);
+
+
+        // Send back the SDP we received from the OpenAI REST API
+        const sdp = await sdpResponse.text();
+        res.send(sdp);
+        initSideBand(callId , req.params.interviewId)
+      } catch (error) {
+        console.error("Token generation error:", error);
+        res.status(500).json({ error: "Failed to generate token" });
+      }
+})
+
 
 app.listen(3001, () => {
     console.log("Server running on port 3001");
---
+++ backend/sideband.ts
@@ -0,0 +1,29 @@
+import WebSocket from "ws";
+
+export function initSideBand(callId : string , interviewId : string){
+    const url = "wss://api.openai.com/v1/realtime?call_id=" + callId;
+    const ws = new WebSocket(url, {
+        headers: {
+            Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+        },
+    });
+    ws.on("open", function open() {
+        console.log("Connected to server.");
+      
+        // Send client events over the WebSocket once connected
+        ws.send(
+          JSON.stringify({
+            type: "session.update",
+            session: {
+              type: "realtime",
+              instructions: `You are supposed to interview this user on their computer science intellect . Ask around 2-3 questions based 
+                on their experience. 
+              `,
+            },
+          })
+        );
+    });
+      ws.on("message", function incoming(message) {
+        console.log(JSON.parse(message.toString()));
+      });
+}
